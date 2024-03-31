99cbfafc4aa22c0fde4f4c44bb957870c8b43250
Ouzinem <ouzinem@hotmail.com>
2024-03-31 11:51:06 -0400

    create web api for better structuration

---
+++ BlazingPizzaSite.API/Program.cs
@@ -0,0 +1,36 @@
+using BlazingPizzaSite.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContextFactory<SimplePizzaCatalogDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "BlazingPizzaSite.API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
