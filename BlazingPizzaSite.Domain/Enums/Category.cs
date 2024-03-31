9cf80a493afa4bfb4fb050508297524ecd04c7e2
Ouzinem <ouzinem@hotmail.com>
2024-03-31 08:16:21 -0400

    Domain Laye : change chicken for Chicken

--- BlazingPizzaSite.Domain/Enums/Category.cs
+++ BlazingPizzaSite.Domain/Enums/Category.cs
@@ -10,7 +10,7 @@ namespace BlazingPizzaSite.Domain.Enums
     {
         // different categories of pizza
         Meat,
-        chicken,
+        Chicken,
         Vegetarian,
         Vegan,
         GlutenFree,
