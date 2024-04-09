61efdaebd79ca3bc777a71b346312f29d7f47a4d
Ouzinem <ouzinem@hotmail.com>
2024-04-09 05:41:27 -0400

    seeeding data
    This reverts commit e39baa6d80e9fa2bb9ef46f7f86fe43c6a8bc790.

--- BlazingPizza2/BlazingPizza2/BlazingPizza2/Program.cs
+++ BlazingPizza2/BlazingPizza2/BlazingPizza2/Program.cs
@@ -1,5 +1,5 @@
-using BlazingPizza2.Client.Pages;
 using BlazingPizza2.Components;
+using BlazingPizza2.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,12 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,16 +25,35 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseRouting();
 app.UseAntiforgery();
+
+app.MapRazorPages();
+app.MapBlazorHub();
+//app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazingPizza2.Client._Imports).Assembly);
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
+
 
 app.Run();
